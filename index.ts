d4bbe61a20743cee129be763c050db1cd56c93b0
portusan2000 <44505282+portusan2000@users.noreply.github.com>
2020-02-13 12:24:20 +0100

    REST server con Typescript

---
+++ index.ts
@@ -0,0 +1,19 @@
+import Server from "./classes/server";
+import router from "./routes/router";
+import express from 'express';
+import cors from 'cors';
+
+const server = new Server();
+const port = server.port;
+
+// Middlewares
+server.app.use(cors({origin: true, credentials: true}))
+server.app.use(express.urlencoded({extended: true}));
+server.app.use(express.json());
+
+// Routes
+server.app.use('/', router);
+
+server.start( () => {
+    console.log(`Servidor corriendo en el puerto: ${port}`);
+})
